0572066ec2ca25b9f731e4b672740d0ac5363803
priyanka-easycampus <priyanka.bedwal@easycampus.ai>
2024-05-24 09:57:21 +0530

    24may changes

--- components/banner/index.jsx
+++ components/banner/index.jsx
@@ -21,13 +21,13 @@ const Herosection = () => {
     <div className='grid grid-cols-1 lg:grid-cols-12 2xl:mx-24 xl:mx-20 lg:mx-20 md:mx-20 sm:mx-10 mx-5 gap-14'> 
       <div className='col-span-1 lg:col-span-7'>
         <div className='mt-[9rem]'>
-          <h1 className='text-[60px] font-bold'>
+          <h1 className=''>
             Ignite Your <span className='text-[#51a9ff]'>Tech Career</span> <br />
             Today: Learn, Grow, and Get Placed
           </h1>
-          <p className='text-[25px] mt-5 banergradienttext'>
+          <h3 className='mt-5 banergradienttext'>
             Full Stack Development | Cyber Security | Data Science & AI
-          </p>
+          </h3>
         </div>
         <div className='grid grid-cols-1 lg:grid-cols-4 border banerlogosection text-center gap-10 '>
           {stats.map((stat, index) => (
@@ -36,7 +36,7 @@ const Herosection = () => {
                 <Image src={stat.logo} alt={`banerlogo${index + 1}`} />
               </div>
               <div className='ml-4 text-start'>
-                <h1 className='text-2xl font-bold'>{stat.value}</h1>
+                <h4>{stat.value}</h4>
                 <p>{stat.label}</p>
               </div>
             </div>
@@ -56,12 +56,12 @@ const Herosection = () => {
       </div>
       <div className='col-span-1 lg:col-span-5 mt-32 mb-5'>
         <div className='bg-[#fff] py-16 px-12 banerformshadow rounded-[30px] text-center'>
-          <h1 className='text-[#000] text-xl font-bold mb-6'>Start learning for <span className='#2f80ed'>FREE</span></h1>
+          <h4 className='mb-6'>Start learning for <span className='#2f80ed'>FREE</span></h4>
           <p className='text-gray-400 pb-8'>Lectures & Assignments curated by Top Tech <br/> Professionals</p>
           <form>
           <div className="mb-4 flex justify-center">
-  <div className="relative w-full">
-    <i className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-6 h-6">
+   <div className="relative w-full">
+    <i className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-8 h-8">
        <svg xmlns="http://www.w3.org/2000/svg"  className=" absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
        <path  d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
      </svg>
@@ -69,17 +69,16 @@ const Herosection = () => {
     <input
       type="text"
       id="fullName"
-      className="shadow appearance-none rounded-[0.30rem] w-full py-6 pl-12 pr-7 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      className="shadow appearance-none rounded-[0.30rem] w-full py-6 pl-14 pr-7 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
       placeholder="Full Name"
     />
   </div>
 </div>
-{/* webpack.js?v=1716407006689:508 Warning: Received `true` for a non-boolean attribute `s`. console error */}
     <div className="mb-6 flex justify-center">
   <div className="relative w-full flex items-center">
     <select
       id="countryCode"
-      className="shadow appearance-none rounded-[0.30rem] py-6 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      className="shadow appearance-none rounded-[0.30rem] py-6 px-4 text-gray-700  leading-tight focus:outline-none focus:shadow-outline"
     >
       <option value="+1">+1</option>  
       <option value="+44">+44</option>
@@ -109,8 +108,8 @@ const Herosection = () => {
                 </button>
               </div>
           </form>
-          <h1 className='mt-5'>By clicking ‘Apply Now For Free’, you agree to our <br/>
-          <span className='text-[#2f80ed]'>Terms & Conditions</span> </h1>
+          <h4 className='mt-5'>By clicking ‘Apply Now For Free’, you agree to our <br/>
+          <span className='text-[#2f80ed]'>Terms & Conditions</span> </h4>
         </div>
       </div>
     </div>
